75e1332027d49cb72d10419379462f192d0a34e4
ganeshblaze <argentinamark@gmail.com>
2019-08-18 12:59:33 +0530

    Updates

---
+++ precache-manifest.5a9abd12d7455919c5a0891fc9e8a343.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "063e3b8a6645d9631a06ebfe77abcc83",
+    "url": "/crudeoper/index.html"
+  },
+  {
+    "revision": "c2557f0303c3b161a40e",
+    "url": "/crudeoper/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1cb50232387d73663a2b",
+    "url": "/crudeoper/static/js/2.e4647524.chunk.js"
+  },
+  {
+    "revision": "c2557f0303c3b161a40e",
+    "url": "/crudeoper/static/js/main.8b579386.chunk.js"
+  },
+  {
+    "revision": "00e1be28a8778bbd9f88",
+    "url": "/crudeoper/static/js/runtime~main.3333f8a5.js"
+  }
+]);
